8d268935757894ac0e354c68fd8b4594eb45bc5b
GoldenEXPn <zty1378212047@gmail.com>
2024-09-27 17:00:29 -0400

    filling part 3, working on part 4 & problem 2

--- HW1/problem_1/train.py
+++ HW1/problem_1/train.py
@@ -10,7 +10,7 @@ from utils import set_seed
 from torch import nn, optim
 from torchvision.models import resnet18
 from torchvision import transforms, datasets
-from torch.utils.data import DataLoader, random_split, Dataset
+from torch.utils.data import DataLoader, Dataset, Subset
 
 
 def get_scheduler(use_scheduler, optimizer, **kwargs):
@@ -22,7 +22,7 @@ def get_scheduler(use_scheduler, optimizer, **kwargs):
     """
     # https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.OneCycleLR.html
     if use_scheduler:
-        raise NotImplementedError
+        scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, **kwargs)
     else:
         scheduler = None
     return scheduler
@@ -35,42 +35,44 @@ def evaluate(model, data_loader, device):
     :param device: cpu or cuda
     :return: accuracy, cross entropy loss (sum)
     """
-    # code below is just a reference, you may modify this part during your implementation
     model.eval()
-    num_instances = 0
-    val_acc = None
-    val_loss = 0.0
+    num_instances, correct_predictions = 0, 0
+    total_loss = 0.0
     criterion = nn.CrossEntropyLoss(reduction='sum')
     with torch.no_grad():
-        raise NotImplementedError
-    model.train()
-    return val_acc, val_loss
+        for inputs, labels in data_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+
+            loss = criterion(outputs, labels)
+            total_loss += loss.item()
+
+            _, preds = torch.max(outputs, 1)
+            correct_predictions += torch.sum(preds == labels)
+            num_instances += labels.size(0)
+    avg_loss = total_loss / num_instances
+    accuracy = correct_predictions / num_instances
+    model.eval()
+    return accuracy, avg_loss, loss.item()
 
 
 class OxfordPetDataset(Dataset):
-    def __init__(self, dataset, csv_file, split='train', transform=None):
-        self.dataset = dataset
-        self.df = pd.read_csv(csv_file)
-        self.df = self.df[self.df['split'] == split]  # Filter by train or test
+    def __init__(self, csv_file, split, label_map, transform=None):
+        self.csv_file = csv_file[csv_file['split'] == split]
+        self.label_map = label_map  # Filter by train or test
         self.transform = transform
 
     def __len__(self):
-        return len(self.df)
+        return len(self.csv_file)
 
     def __getitem__(self, idx):
-        # Get image
-        img_name = self.df.iloc[idx]['image_name']
-        img_idx = self.dataset._filename_to_idx[img_name]  # Map filename to index in the dataset
-        image = self.dataset.images[img_idx]
+        img_name = self.csv_file.iloc[idx]['image_name']
+        img_path = f"data/oxford-iiit-pet/images/{img_name}"
+        label = self.csv_file.iloc[idx]['label']
+        label = self.label_map[label]
 
-        # Image tansform
-        image = Image.open(image).convert('RGB')
-        if self.transform:
-            image = self.transform(image)
-
-        # Get the label
-        label = self.df.iloc[idx]['label']
-
+        image = Image.open(img_path).convert('RGB')
+        if self.transform: image = self.transform(image)
         return image, label
 
 
@@ -83,20 +85,20 @@ def train_model(
         device,
         save_dir,
         use_scheduler,
+        rid
 ):
     model.to(device)
 
-    # Load dataset
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Using imagenet std and mean
-    ])
-    dataset = datasets.OxfordIIIPet(root='data', download=True, transform=transform)
+    # Load dataset and csv file
+    transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])  # Use ImageNet
+    dataset = datasets.OxfordIIITPet(root='data', download=True, transform=transform)
     df = pd.read_csv('oxford_pet_split.csv')
-    train_set = OxfordPetDataset(dataset, df, split="train", transform = transform)
-    val_set = OxfordPetDataset(dataset, df, split="split", transform = transform)
-    test_set = OxfordPetDataset(dataset, df, split="val", transform = transform)
+    # Image-label mapping
+    labels = df['label'].unique()
+    label_map = {label: idx for idx, label in enumerate(labels)}
+    train_set = OxfordPetDataset(df, 'train', label_map, transform)
+    val_set = OxfordPetDataset(df, 'val', label_map, transform)
+    test_set = OxfordPetDataset(df, 'test', label_map, transform)
 
     n_train, n_val, n_test = len(train_set), len(val_set), len(test_set)
     loader_args = dict(batch_size=batch_size, num_workers=4)
@@ -109,15 +111,21 @@ def train_model(
 
     # Initialize a new wandb run and log experiment config parameters; don't forget the run name
     # you can also set run name to reflect key hyperparameters, such as learning rate, batch size, etc.: run_name = f'lr_{learning_rate}_bs_{batch_size}...'
-    wandb.init(project="277 hw1", name=run_name, config={
-        "epoch": epochs,
-        "learning_rate": learning_rate,
-        "batch_size": batch_size,
-    })
-
+    wandb.init(
+        project="277 hw1",
+        name=run_name,
+        config={
+            "epoch": epochs,
+            "learning_rate": learning_rate,
+            "batch_size": batch_size,
+            "scheduler": use_scheduler,
+            "total_training_steps": total_training_steps,
+            "model": "ResNet-18"
+        }
+    )
 
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    scheduler = get_scheduler(use_scheduler, optimizer, max_lr=learning_rate, total_steps=total_training_steps, pct_start=0.1, final_div_factor=10)
+    scheduler = get_scheduler(use_scheduler, optimizer, max_lr=learning_rate * 10, total_steps=total_training_steps, pct_start=0.3, final_div_factor=10)  # At 30% of data we reach max_lr
 
     criterion = nn.CrossEntropyLoss()
 
@@ -139,16 +147,29 @@ def train_model(
                 optimizer.step()
                 if use_scheduler:
                     scheduler.step()
+                    cur_lr = scheduler.get_last_lr()[0]
+                else:
+                    cur_lr = optimizer.param_groups[0]['lr']
                 pbar.update(inputs.shape[0])
                 global_step += 1
                 # save necessary metrics in a dictionary; it's recommended to also log seen_examples, which helps you creat appropriate figures in Part 3
                 # code here
+                metrics = {
+                    'train_loss': loss.item(),
+                    # 'seen_examples': seen_examples,
+                }
+                wandb.log(metrics)
 
                 if global_step % batch_steps == 0:
                     # evaluate on validation set
-                    val_acc, val_loss = evaluate(model, val_loader, device)
+                    val_acc, val_loss, val_loss_step = evaluate(model, val_loader, device)
                     # update metrics from validation results in the dictionary
                     # code here
+                    metrics.update({
+                        # 'val_loss': val_loss,
+                        'val_acc': val_acc,
+                    })
+                    wandb.log(metrics)
 
                     if best_val_loss > val_loss:
                         best_val_loss = val_loss
@@ -156,19 +177,71 @@ def train_model(
                         state_dict = model.state_dict()
                         torch.save(state_dict, os.path.join(save_dir, f'{run_name}_{rid}', 'checkpoint.pth'))
                         print(f'Checkpoint at step {global_step} saved!')
-                # log metrics to wandb
-                # code here
+                    # log metrics to wandb
+                    # code here
+                    wandb.log({'learning_rate': cur_lr})
 
                 pbar.set_postfix(**{'loss (batch)': loss.item()})
+        wandb.log({'epoch': epoch})
 
     # load best checkpoint and evaluate on test set
     print(f'training finished, run testing using best ckpt...')
     state_dict = torch.load(os.path.join(save_dir, f'{run_name}_{rid}', 'checkpoint.pth'))
     model.load_state_dict(state_dict)
-    test_acc, test_loss = evaluate(model, test_loader, device)
+    test_acc, test_loss, test_loss_step = evaluate(model, test_loader, device)
 
     # log test results to wandb
     # code here
+    wandb.log({'final_test_acc': test_acc, 'final_test_loss_step': test_loss_step})
+    wandb.summary.update({"final_test_accuracy": test_acc, "final_test_loss": test_loss})
+    wandb.finish()
+    print(f'test acc: {test_acc}, test loss: {test_loss}')
+
+
+sweep_config = {
+    "name": "Tuning with sweep",
+    "method": "grid",  # Ensure each learning rate is picked only once
+    "metric": {"goal": "minimize", "name": "validation_loss"},
+    "parameters": {
+        "learning_rate": {
+            'values': [1e-2, 1e-3, 1e-4, 1e-5]
+        },
+        "batch_size": {
+            "values": [32]
+        },
+        "epochs": {
+            "values": [10]
+        },
+    },
+}
+def sweep_train():
+    wandb.init()
+    config = wandb.config
+
+    # setup run_name
+    run_name = f'lr_{config.learning_rate}_bs_{config.batch_size}_epochs_{config.epochs}'
+    wandb.run.name = run_name
+    wandb.run.save()
+
+    # Initialize model
+    set_seed(42)
+    rid = random.randint(0, 1000000)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = resnet18(pretrained=False, num_classes=37).to(device)
+
+    # Train model
+    train_model(
+        run_name=run_name,
+        model=model,
+        batch_size = config.batch_size,
+        epochs = config.epochs,
+        learning_rate = config.learning_rate,
+        device=device,
+        save_dir='./checkpoints',
+        use_scheduler=False,
+        rid=rid
+    )
+    wandb.finish()
 
 
 def get_args():
@@ -186,14 +259,13 @@ def get_args():
     parser.add_argument('--batch_size', type=int, default=32, help='batch size; modify this to fit your GPU memory')
     parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
     parser.add_argument('--use_scheduler', action='store_true', help='use lr scheduler')
-
-    # IMPORTANT: if you are copying this script to notebook, replace 'return parser.parse_args()' with 'args = parser.parse_args("")'
-
     return parser.parse_args()
 
 
 
 if __name__ == '__main__':
+
+    # 1.1 & 1.22 OneCycleLR Scheduler
     rid = random.randint(0, 1000000)
     set_seed(42)
     args = get_args()
@@ -208,4 +280,12 @@ if __name__ == '__main__':
         device=device,
         save_dir=args.save_dir,
         use_scheduler=args.use_scheduler,
+        rid=rid
     )
+
+
+    # 1.21 Sweep Config
+    # rid = random.randint(0, 1000000)
+    # set_seed(42)
+    # sweep_id = wandb.sweep(sweep = sweep_config, project="oxford_pet_classification_sweep")
+    # wandb.agent(sweep_id, function=sweep_train)
